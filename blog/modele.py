67cc66f002a59c49bbe5351ab668bdf52e438c28
keitamoustapha913 <keitamoustapha913@gmail.com>
2021-03-27 17:40:05 +0100

    added mongo db , to do add post

--- blog/modele.py
+++ blog/modele.py
@@ -3,6 +3,40 @@ from passlib.hash import bcrypt
 from datetime import datetime
 import os
 import uuid
+import pymongo as pm
+
+
+# Connect to Mongo database
+#mongo_local = pm.MongoClient('localhost', 27017)
+#print(f"\n\n List of mongo databases : {mongo_local.list_database_names()}\n\n")
+
+
+class MongoDb:
+    def __init__(self, pymongo = None, uri= 'localhost', port = 27017):
+        self.port = port
+        self.uri = uri
+        self.pymongo = pymongo
+        self.connect_mongo()
+
+    def connect_mongo(self):
+        self.connection = self.pymongo.MongoClient(self.uri, self.port )
+        print(f"\n\n List of mongo databases : {self.connection.list_database_names()}\n\n")
+
+    def create_database(self, database_name = ""):
+        # Creating an new mongo database
+        self.database = self.connection[database_name]
+
+        return self.database
+
+    def create_collection(self, database_name = "" ,collection_name = ""):
+        if not self.create_database(database_name =database_name):
+            self.database = create_database( database_name = database_name)
+        
+        ## creating a collection
+        self.collection = self.database[collection_name]
+        return self.collection
+
+mongo = MongoDb(pymongo = pm, uri= 'localhost', port = 27017)
 
 
 graph = Graph("bolt://localhost:7687", user="keitaneo4j", password="keitaneo4j")
@@ -12,16 +46,26 @@ class User:
         self.username = username
         self.email=email
         self.sex=sex
+        self.mongo_collection = mongo.create_collection(database_name = "mongo_blog" , collection_name = "User_blog")
 
     def find(self):
         matcher = NodeMatcher(graph)
         user = matcher.match("User", username=self.username).first()
+
+        mongo_filter = {'username' : self.username }
+        mongo_user = self.mongo_collection.find_one( mongo_filter )
+        print(f"\n\n Mongo find user : { mongo_user } ")
+
         return user
 
     def register(self, password, email, sex):
         if not self.find():
             user = Node('User', username=self.username, password=bcrypt.encrypt(password), email=self.email, sex=self.sex)
             graph.create(user)
+
+            input_dict = {'username' : self.username , 'password' : bcrypt.encrypt(password) , 'email' : self.email , "sex" : self.sex }
+            self.mongo_collection.insert_one( input_dict )
+
             return True
         else:
             return False
@@ -29,6 +73,10 @@ class User:
     def verify_password(self, password ):
         user = self.find()
         if user:
+            mongo_filter = {'username' : self.username }
+            mongo_user = self.mongo_collection.find_one( mongo_filter )
+            print(f"\n\n Mongo verify_password : { bcrypt.verify(password, mongo_user['password']) } ")
+
             return bcrypt.verify(password, user['password'])
         else:
             return False
@@ -45,6 +93,13 @@ class User:
         )
         rel = Relationship(user, 'PUBLISHED', post)
         graph.create(rel)
+
+        new_post = { 'Post.id' : str(uuid.uuid4()) , 'Post.title': title, 'Post.tags': tags, 'Post.text': text , 'Post.timestamp': timestamp(), 'Post.date': date()}
+        updated_user = {"$set": new_post  }
+
+        mongo_filter = {'username' : self.username }
+        mongo_user = self.mongo_collection.update_one( mongo_filter, updated_user)
+
 
         tags = [x.strip() for x in tags.lower().split(',')]
         for tag in set(tags):
